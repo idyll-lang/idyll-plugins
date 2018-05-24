15edab377b8dca6369530228bda9c2a8bfc1d861
Pavan Kumar <pavank1@uw.edu>
2018-05-24 15:10:29 -0700

    textcontainer wrap, ul/li, and user can use [/TableOfContents]

--- table-of-contents/index.js
+++ table-of-contents/index.js
@@ -1,23 +1,24 @@
 const AST = require('idyll-ast');
 
-
-// use getNodesbyName(h1) to get all the major headings 
-// loop over and add 
 module.exports = (ast) => {
     let headings = AST.getNodesByName(ast, 'h1');
-    let tagNames = [AST.createNode('h2',{id: 'tableofcontents'}, ['Table of Contents'])];
+    let tags = ['Table of Contents'];
     headings.forEach((node) => {
         let text = AST.getText(node);
         let href = '#' + text.replace(/['"]+/g, '');
-        tagNames.push(AST.createNode('a', {href}, [text]));
-        tagNames.push(AST.createNode('br', {}, []))
+        let element = AST.createNode('a', {href}, [text])
+        tags.push(AST.createNode('li', {}, [element]));
     });
-
+    let tagNames = AST.createNode('h2',{id: 'tableofcontents'}, tags);
+    let list = AST.createNode('ul', {id: 'list'}, [tagNames])
     let ASTwithID = AST.modifyNodesByName(ast, 'h1', (node) => {
         node = AST.setProperty(node, 'id', AST.getText(node))
         return node;
     });
-    
-    ASTwithID = AST.prependNodes(ASTwithID, tagNames)
+
+    let textContainer = AST.createNode('TextContainer', {}, [list])
+    ASTwithID = AST.modifyNodesByName(ASTwithID, 'TableOfContents', (node) => {
+        return textContainer;
+    });
     return ASTwithID;
 };

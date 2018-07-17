9ed9375f22575ab3b6460f515e0e34f40adfbb09
Matthew Conlen <mc@mathisonian.com>
2018-07-17 16:50:45 -0700

    add spellcheck plugin

---
+++ spellcheck/index.js
@@ -0,0 +1,11 @@
+const AST = require('idyll-ast');
+const Spellchecker = require('spellchecker');
+
+module.exports = (ast) => {
+  const text = AST.getText(ast);
+  const misspellings = Spellchecker.checkSpelling(text);
+
+  misspellings.forEach(({start, end}) => {
+    console.warn(`Found misspelling: ${text.substring(start, end)}`);
+  })
+};
